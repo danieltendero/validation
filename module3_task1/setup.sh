c0017fe7bd52dbc939f88a53f0ef96ef10f723b9
daniel <daniel.tendero.tapial@devoteam.com>
2022-02-03 12:35:50 +0100

    subida

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -4,13 +4,15 @@ sudo tar -xzf go1.8.linux-amd64.tar.gz -C /usr/local
 export PATH=$PATH:/usr/local/go/bin
 rm go1.8.linux-amd64.tar.gz
 
-# binary will be $(go env GOPATH)/bin/golangci-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.44.0
-
-
 
 sudo wget https://github.com/gohugoio/hugo/releases/download/v0.87.0/hugo_0.87.0_Linux-64bit.deb
 sudo dpkg -i hugo_0.87.0_Linux-64bit.deb
 sudo rm hugo_0.87.0_Linux-64bit.deb
+
+
+# binary will be $(go env GOPATH)/bin/golangci-lint
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.44.0
+
+golangci-lint --version
 
 sleep 5
